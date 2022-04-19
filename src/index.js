5a33b7a5b288c4b3d674994e41e25bf1a60d9b36
lisa jenkins <jenkinsl8@gmail.com>
2022-04-19 02:47:29 -0400

    added comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+/*
+A custom map style. The was copied from the google maps API demo.
+ The featureType defines the items that will also appear on the map.
+ Features are geographic characteristics on the map, including roads, parks, bodies of water, and more.
+    Roads: selects all roads. Highway, Arterial, local roads are colored.  Color is a RGB value.
+    Administrative: selects all administrative areas. Country, locality, province, land parcels and neighborhoods
+    Landscape: selects all landscapes including manmade, terrain and natural
+    Water: all bodies of water
+    poi: selects point of interests: Parks
+
+ Stylers are formatting options that you can apply to map features and elements.
+ Visibility indicates whether or not a feature will be visible.
+ Lightness (a floating point value between -100 and 100) indicates the percentage change in brightness of the element.
+
+*/
+
 const mapStyle = [{
   'featureType': 'administrative',
   'elementType': 'all',
@@ -89,16 +105,36 @@ function sanitizeHTML(strings) {
 }
 
 function initMap() {
+/*
+create the custom google map. The map is created with a point that exists in the file as the starting point.
+*/
+
+
   const map = new google.maps.Map(document.getElementById('map'), {
       zoom: 16,
       center: {lat: 37.7873042488646, lng: -122.39803725191237},
       styles: mapStyle,
     });
 
+/*
+    loads the GeoJson file and sets the id to be the locationid.
+    Because this uses the Google Map API to load the data, there may be restrictions on the file size
+    and the speed with which the data is loaded.  The index.html calls this logic asynchronously and waits for the map
+    data to load before rendering the map.
+
+    Better approach would be to use Promises
+*/
+
+
   map.data.loadGeoJson('mobileFood.json', {idPropertyName: 'locationid'});
 
 const apiKey = 'AIzaSyBEkAEoZrn9OTlDKt18piNYhPR7qzJ2RRM';
   const infoWindow = new google.maps.InfoWindow();
+/*
+Listens for a mouse click event, when a mouse click occurs, open an information window that shows an icon indication the FacilityType,
+the Applicant, LocationDescription and FoodItems served.  The window will point to the GeoLocation (longitutude, latitude)
+when open.
+*/
 
   map.data.addListener('click', (event) => {
     const category = event.feature.getProperty('FacilityType').replace(" ", "");
@@ -106,6 +142,11 @@ const apiKey = 'AIzaSyBEkAEoZrn9OTlDKt18piNYhPR7qzJ2RRM';
     const description = event.feature.getProperty('LocationDescription');
     const menu = event.feature.getProperty('FoodItems');
     const position = event.feature.getGeometry().get();
+
+/*
+    Strip special chars from the data for HTML display and build the info window content based on the HTML
+*/
+
     const content = sanitizeHTML`
       <img style="float:left; width:200px; margin-top:30px" src="img/icon_${category}.png">
       <div style="margin-left:220px; margin-bottom:20px;">
@@ -116,9 +157,18 @@ const apiKey = 'AIzaSyBEkAEoZrn9OTlDKt18piNYhPR7qzJ2RRM';
 
     infoWindow.setContent(content);
     infoWindow.setPosition(position);
+
+/*
+    pixelOffset contains an offset from the tip of the info window to the location on which the info window is anchored.
+*/
+
     infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
     infoWindow.open(map);
   });
+
+/*
+Recenter the map post load
+*/
 
  var originLocation = map.getCenter();
  map.setCenter(originLocation);
--- src/readCSV.js
+++ src/readCSV.js
@@ -1,11 +1,27 @@
+/*
+uses csv-parse and fs to open and read the CSV file.
+geojson is used to convert the cvs data into a geojson file that is required by the Google Maps Javascript API.
+Writes the file out to the current directory. */
+
 function ReadCSV() {
 const fs = require('fs');
 const {parse} = require('csv-parse');
 const GeoJSON  = require('geojson');
 var writeStream = fs.createWriteStream('./mobileFood.json');
 
+/*
+parser receives the records read in from fs as a collection of records.  Passes the records to GeoJSON function to
+generate the output file
+*/
+
 const parser = parse({columns: true}, function (err, records) {
      console.log(records);
+
+/*
+parses the csv records read, creates the coord format using Lat/long values from the csv file:
+For more info about the GeoJSON format info see: https://geojson.org/
+*/
+
     GeoJSON.parse(records, {Point: [ 'Latitude', 'Longitude']}, function(geojson){
              console.log(JSON.stringify(geojson));
              writeStream.write(JSON.stringify(geojson));
@@ -14,6 +30,7 @@ const parser = parse({columns: true}, function (err, records) {
 
 });
 
+// reads the csv data as an input stream and pipes the data read to parser defined above.
 fs.createReadStream(__dirname+'/resources/Mobile_Food_Facility_Permit.csv').pipe(parser);
 }
 
